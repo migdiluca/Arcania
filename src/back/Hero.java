e24cf63e31da9844e7db63434756368ac8b1c011
fernando <femartin@itba.edu.ar>
2017-11-13 16:04:01 -0300

    SERIALIZACION

--- src/back/Hero.java
+++ src/back/Hero.java
@@ -1,8 +1,14 @@
 package back;
 
+import java.awt.*;
+
 public class Hero extends Soldier {
     public Hero(String name, int id, int attack, int health, int defense, int agility, String description){
         super(name,id,attack,health,defense,agility, description);
+    }
+
+    public void throwMagic(Magic m){
+        m.effect();
     }
 
 }
