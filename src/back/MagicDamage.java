e01ee538b86839f8627eb51c3c4d04670f0d5cff
nico_ <nbarreara@itba.edu.ar>
2017-11-17 01:35:38 -0300

    Arrego

--- src/back/MagicDamage.java
+++ src/back/MagicDamage.java
@@ -5,6 +5,10 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
+/**
+ * Clase que modela los hechizos que producen un daño instantáneo
+ */
+
 public class MagicDamage extends Magic implements Serializable {
     private int damage;
     public MagicDamage(String name, int id, String description, boolean isNegative, int damage){
